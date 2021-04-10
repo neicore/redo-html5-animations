460f273cd2c35391a11146d20f488d3d50710c89
neicore <neicoreadams@gmail.com>
2021-04-10 15:09:33 +0300

    Modifying animation delay

--- script.js
+++ script.js
@@ -66,7 +66,7 @@ let connection_n_2 = document.querySelector('.Stage_titles_alt_nCopy2_id');
 let connection_n_2_alt = document.querySelector('.Stage_titles_n_3_id');
 
 //Function for changing letters
-function changeLetter(currentLetter, replacingLetter, posX_1, posY_1, posX_2, posY_2) {
+function changeLetter(currentLetter, replacingLetter, posX_1, posY_1, posX_2, posY_2, delay) {
     replacingLetter.style.display = 'block';
     currentLetter.style.display = 'block';
 
@@ -74,12 +74,14 @@ function changeLetter(currentLetter, replacingLetter, posX_1, posY_1, posX_2, po
         targets: currentLetter,
         backgroundPosition: `${posX_1}px ${posY_1}px`,
         easing: 'easeInOutSine',
+        delay: delay,
     });
 
     anime({
         targets: replacingLetter,
         backgroundPosition: `${posX_2}px ${posY_2}px`,
         easing: 'easeInOutSine',
+        delay: delay,
     });
 }
 
@@ -92,19 +94,19 @@ function changeBackgroundPosition(letter, positionX, positionY) {
     });
 }
 
-changeLetter(my_m_alt, my_m, 80, 0, 0, 0);
-changeLetter(spot_p_alt, spot_p, 75, 0, 0, 0);
-changeLetter(spot_o_alt, spot_o, 80, 0, 0, 0);
-changeLetter(connection_c_0_alt, connection_c_0, 0, 70, 0, 0);
-changeLetter(connection_o_0_alt, connection_o_0, 0, -53, 0, 0);
-changeLetter(connection_n_0_alt, connection_n_0, -40, 0, 0, 0);
-changeLetter(connection_n_1_alt, connection_n_1, -40, 0, 0, 0);
-changeLetter(connection_e_alt, connection_e, 40, 0, 0, 0);
-changeLetter(connection_c_1_alt, connection_c_1, 0, -55, 0, 0);
-changeLetter(connection_t_alt, connection_t, -30, 0, 0, 0);
-changeLetter(connection_i_alt, connection_i, 25, 0, 0, 0);
-changeLetter(connection_o_1_alt, connection_o_1, 0, -53, 0, 0);
-changeLetter(connection_n_2_alt, connection_n_2, 0, 52, 0, 0);
+changeLetter(my_m_alt, my_m, 80, 0, 0, 0, 0);
+changeLetter(spot_p_alt, spot_p, 75, 0, 0, 0, 0);
+changeLetter(spot_o_alt, spot_o, 80, 0, 0, 0, 0);
+changeLetter(connection_c_0_alt, connection_c_0, 0, 70, 0, 0, 0);
+changeLetter(connection_o_0_alt, connection_o_0, 0, -53, 0, 0, 0);
+changeLetter(connection_n_0_alt, connection_n_0, -40, 0, 0, 0, 0);
+changeLetter(connection_n_1_alt, connection_n_1, -40, 0, 0, 0, 0);
+changeLetter(connection_e_alt, connection_e, 40, 0, 0, 0, 0);
+changeLetter(connection_c_1_alt, connection_c_1, 0, -55, 0, 0, 0);
+changeLetter(connection_t_alt, connection_t, -30, 0, 0, 0, 0);
+changeLetter(connection_i_alt, connection_i, 25, 0, 0, 0, 0);
+changeLetter(connection_o_1_alt, connection_o_1, 0, -53, 0, 0, 0);
+changeLetter(connection_n_2_alt, connection_n_2, 0, 52, 0, 0, 0);
 
 // Scaling all titles to 1
 anime({
@@ -117,22 +119,22 @@ anime({
     delay: anime.stagger(100, { start: 700 }),
 });
 
+// starting animation
 let timeOut = 3000;
-let timeoutIncrement = 3000;
 
 function callThem() {
     setTimeout(() => {
-        changeLetter(my_m_alt, my_m, 0, 0, -80, 0,);
-        changeLetter(my_y_alt, my_y, 0, 0, 50, 0,);
-        changeLetter(friend_alt, friend, 0, 0, 0, 80,);
+        changeLetter(my_m_alt, my_m, 0, 0, -80, 0, 0);
+        changeLetter(my_y_alt, my_y, 0, 0, 50, 0, 0);
+        changeLetter(friend_alt, friend, 0, 0, 0, 80, 0);
     }, timeOut);
 
     setTimeout(() => {
-        changeLetter(spot_s_alt, spot_s, 0, 0, 75, 0);
-        changeLetter(spot_p, spot_p_alt, -75, 0, 0, 0);
-        changeLetter(spot_o, spot_o_alt, -80, 0, 0, 0);
-        changeLetter(spot_t_alt, spot_t, 0, 0, 0, 100);
-        changeLetter(your_alt, your, 0, 0, 0, 80);
+        changeLetter(spot_s_alt, spot_s, 0, 0, 75, 0, 0);
+        changeLetter(spot_p, spot_p_alt, -75, 0, 0, 0, 0);
+        changeLetter(spot_o, spot_o_alt, -80, 0, 0, 0, 0);
+        changeLetter(spot_t_alt, spot_t, 0, 0, 0, 100, 0);
+        changeLetter(your_alt, your, 0, 0, 0, 80, 0);
     }, timeOut + 2000);
 
     setTimeout(() => {
@@ -157,32 +159,32 @@ function callThem() {
     }, timeOut + 3000);
 
     setTimeout(() => {
-        changeLetter(my_m_alt, my_m, 80, 0, 0, 0);
-        changeLetter(my_y_alt, my_y, 80, 0, 0, 0);
+        changeLetter(my_m_alt, my_m, 80, 0, 0, 0, 0);
+        changeLetter(my_y_alt, my_y, 80, 0, 0, 0, 0);
 
-        changeLetter(spot_s_alt, spot_s, 80, 0, 0, 0);
-        changeLetter(spot_p_alt, spot_p, 80, 0, 0, 0);
-        changeLetter(spot_o_alt, spot_o, 80, 0, 0, 0);
-        changeLetter(spot_t_alt, spot_t, 80, 0, 0, 0);
+        changeLetter(spot_s_alt, spot_s, 80, 0, 0, 0, 0);
+        changeLetter(spot_p_alt, spot_p, 80, 0, 0, 0, 0);
+        changeLetter(spot_o_alt, spot_o, 80, 0, 0, 0, 0);
+        changeLetter(spot_t_alt, spot_t, 80, 0, 0, 0, 0);
     }, timeOut + 3500);
 
     setTimeout(() => {
-        changeLetter(your_alt, your, -250, 0, 0, 0);
-        changeLetter(friend_alt, friend, -350, 0, 0, 0);
+        changeLetter(your_alt, your, -250, 0, 0, 0, 1000);
+        changeLetter(friend_alt, friend, -350, 0, 0, 0, 2000);
     }, timeOut + 4500);
 
     setTimeout(() => {
-        changeLetter(connection_c_0, connection_c_0_alt, 0, 80, 0, 0);
-        changeLetter(connection_o_0, connection_o_0_alt, 0, 80, 0, 0);
-        changeLetter(connection_n_0, connection_n_0_alt, 0, 80, 0, 0);
-        changeLetter(connection_n_1, connection_n_1_alt, 0, 80, 0, 0);
-        changeLetter(connection_e, connection_e_alt, 0, 80, 0, 0);
-        changeLetter(connection_c_1, connection_c_1_alt, 0, 80, 0, 0);
-        changeLetter(connection_t, connection_t_alt, 0, 80, 0, 0);
-        changeLetter(connection_i, connection_i_alt, 0, 80, 0, 0);
-        changeLetter(connection_o_1, connection_o_1_alt, 0, 80, 0, 0);
-        changeLetter(connection_n_2, connection_n_2_alt, 0, 80, 0, 0);
-    }, timeOut + 6000);
+        changeLetter(connection_c_0, connection_c_0_alt, 0, 80, 0, 0, 0);
+        changeLetter(connection_o_0, connection_o_0_alt, 0, 80, 0, 0, 0);
+        changeLetter(connection_n_0, connection_n_0_alt, 0, 80, 0, 0, 0);
+        changeLetter(connection_n_1, connection_n_1_alt, 0, 80, 0, 0, 0);
+        changeLetter(connection_e, connection_e_alt, 0, 80, 0, 0, 0);
+        changeLetter(connection_c_1, connection_c_1_alt, 0, 80, 0, 0, 0);
+        changeLetter(connection_t, connection_t_alt, 0, 80, 0, 0, 0);
+        changeLetter(connection_i, connection_i_alt, 0, 80, 0, 0, 0);
+        changeLetter(connection_o_1, connection_o_1_alt, 0, 80, 0, 0, 0);
+        changeLetter(connection_n_2, connection_n_2_alt, 0, 80, 0, 0, 0);
+    }, timeOut + 8000);
 
     setTimeout(() => {
         changeBackgroundPosition(connection_c_0, 80, 0);
@@ -195,36 +197,33 @@ function callThem() {
         changeBackgroundPosition(connection_i, 80, 0);
         changeBackgroundPosition(connection_o_1, 80, 0);
         changeBackgroundPosition(connection_n_2, 80, 0);
-    }, timeOut + 7000);
+    }, timeOut + 9000);
 
     setTimeout(() => {
-        changeLetter(connection_c_0, connection_c_0_alt, 0, 0, -80, 0);
-        changeLetter(connection_o_0, connection_o_0_alt, 0, 0, -80, 0);
-        changeLetter(connection_n_0, connection_n_0_alt, 0, 0, -80, 0);
-        changeLetter(connection_n_1, connection_n_1_alt, 0, 0, -80, 0);
-        changeLetter(connection_e, connection_e_alt, 0, 0, -80, 0);
-        changeLetter(connection_c_1, connection_c_1_alt, 0, 0, -80, 0);
-        changeLetter(connection_t, connection_t_alt, 0, 0, -80, 0);
-        changeLetter(connection_i, connection_i_alt, 0, 0, -80, 0);
-        changeLetter(connection_o_1, connection_o_1_alt, 0, 0, -80, 0);
-        changeLetter(connection_n_2, connection_n_2_alt, 0, 0, -80, 0);
-    }, timeOut + 8000);
+        changeLetter(connection_c_0, connection_c_0_alt, 0, 0, -80, 0, 100);
+        changeLetter(connection_o_0, connection_o_0_alt, 0, 0, -80, 0, 0);
+        changeLetter(connection_n_0, connection_n_0_alt, 0, 0, -80, 0, 0);
+        changeLetter(connection_n_1, connection_n_1_alt, 0, 0, -80, 0, 0);
+        changeLetter(connection_e, connection_e_alt, 0, 0, -80, 0, 0);
+        changeLetter(connection_c_1, connection_c_1_alt, 0, 0, -80, 0, 0);
+        changeLetter(connection_t, connection_t_alt, 0, 0, -80, 0, 0);
+        changeLetter(connection_i, connection_i_alt, 0, 0, -80, 0, 0);
+        changeLetter(connection_o_1, connection_o_1_alt, 0, 0, -80, 0, 0);
+        changeLetter(connection_n_2, connection_n_2_alt, 0, 0, -80, 0, 0);
+    }, timeOut + 10000);
 }
 
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
+callThem();
+
+// Loop all animations
+// function sleep(ms) {
+//     return new Promise(resolve => setTimeout(resolve, ms));
+//   }
   
-    
+// async function run_in_loop(params) {
+//     callThem();
+//     await sleep(13000);
+//     run_in_loop();
+// }
 
-async function run_in_loop(params) {
-    callThem();
-    console.log("hello world")
-    await sleep(11000);
-    console.log("something going on")
-    run_in_loop();
-}
-
-run_in_loop();
-
-// starting animation
+// run_in_loop();
